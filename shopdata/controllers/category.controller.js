a0d3fbaef2424a7d715ce23f4f317a080846cca8
Morning <morning@admins-iMac.local>
2023-03-23 12:12:43 +0800

    aa

--- shopdata/controllers/category.controller.js
+++ shopdata/controllers/category.controller.js
@@ -4,50 +4,50 @@ const fs = require('fs');
 const uuid = require('uuid');
 
 const file = process.cwd() + "/data/category.json"
-const Category = require("../models/category.model");
+const Category = require("../models/Category.model");
 
 exports.getAll = async (req, res) => {
-    try{
+    try {
         const result = await Category.find({});
-        res.json({status: true , result});
-    }catch(err){
-        res.json({status: false , message: err});
+        res.json({ status: true, result });
+    } catch (err) {
+        res.json({ status: false, message: err });
     }
 };
 
 exports.get = async (req, res) => {
     const { _id } = req.params;
-    try{
-        const result = await Category.findById({_id});
-        res.json({status: true , result});
-    }catch(err){
-        res.json({status: false , message: err});
+    try {
+        const result = await Category.findById({ _id });
+        res.json({ status: true, result });
+    } catch (err) {
+        res.json({ status: false, message: err });
     };
 };
 
 exports.uptade = async (req, res) => {
     const { _id } = req.params;
-    try{
-        const result = await Category.findByIdAndUpdate({_id} , req.body);
-    }catch(err){
-        res.json({status: false , message: err});
+    try {
+        const result = await Category.findByIdAndUpdate({ _id }, req.body);
+    } catch (err) {
+        res.json({ status: false, message: err });
     };
 };
 
 exports.create = async (req, res) => {
-    try{
+    try {
         const result = await Category.create(req.body);
-    }catch(err){
-        res.json({status:false , message: err});
+    } catch (err) {
+        res.json({ status: false, message: err });
     };
 };
 
 exports.delete = async (req, res) => {
     const { _id } = req.params;
-    try{
-        const result = await Category.findByIdAndDelete({_id})
-        res.json({status: true , result})
-    }catch(err){
-        res.json({status: false , message: err});
+    try {
+        const result = await Category.findByIdAndDelete({ _id })
+        res.json({ status: true, result })
+    } catch (err) {
+        res.json({ status: false, message: err });
     };
 };
